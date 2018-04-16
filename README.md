1905c169b81434b0f822fffec2d7327cc347658f
Andre Weissflog <floooh@gmail.com>
2018-04-16 17:57:04 +0200

    add launch.json for VSCode debugging

---
+++ README.md
@@ -0,0 +1,11 @@
+With the Typescript compiler and node in the path:
+
+```
+> npm install
+> tsc
+> node bin/http-test.js
+```
+
+Then open browser and navigate to localhost:8080.
+
+...or use VSCode for a nice edit/compile/test loop :)
--- http-test.ts
+++ http-test.ts
@@ -2,6 +2,7 @@ import * as http from "http";
 
 console.log('Listening on localhost:8080');
 http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
+    console.log("HTTP request received!");
     response.write('Hello World!');
     response.end();
 }).listen(8080);
